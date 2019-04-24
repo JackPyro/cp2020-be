15b41cddd01c50b7ddfd2f9018256fb92edc1aec
Дядя Толя <ncorovaneer@gmail.com>
2019-04-24 23:24:36 +0700

    Weapons CRUD hotfix

--- routes/compendium.ts
+++ routes/compendium.ts
@@ -3,8 +3,10 @@ import * as Router from 'koa-router';
 import { findWeaponsByName } from '../controllers/compendium/findWeaponsByName';
 import { addWeapon } from '../controllers/compendium/addWeapon';
 import { deleteWeaponById } from '../controllers/compendium/deleteWeapon';
+import {listWeapons} from '../controllers/compendium/listWeapons';
 export const router = new Router();
 
-router.post('/weapons/', findWeaponsByName);
+router.post('/weapons/', listWeapons);
+router.post('/weapons/find', findWeaponsByName);
 router.post('/weapons/add', addWeapon);
 router.post('/weapons/delete', deleteWeaponById);
